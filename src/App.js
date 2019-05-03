2681fbfbe2392e5074acd52975b4d141c845c924
d3v833 <d3v833@gmail.com>
2019-05-04 03:03:16 +0800

    json placeholder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
 import CardList from './cardList';
 import SearchBox from './searchBox';
-import { people } from './people';
 import './index.css';
 import './App.css';
+class App extends Component {
+  constructor() {
+    super() // whenever you extend a class, you need super to access it
+    this.state = {
+      people: [],
+      searchfield: ''
+    }
+  }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=> response.json())
+    .then(users=> this.setState({ people: users}));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
+  render() {
+    const filteredCatpeople = this.state.people.filter(people =>{
+      return people.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    })
+    if (this.state.people.length === 0) {
+      return <h1>Loading</h1>
+    }
+    return (
+      <div className="tc">
+        <h1 className="f3 f-headline-l">C A T  P E O P L E</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList people={ filteredCatpeople }/>
+      </div>
+    );
+  } 
+}
 
 const changeBg = (color) => {
   document.documentElement.style.setProperty('--background', color)
@@ -16,37 +49,5 @@ onmousemove = (e) => {
   console.log(valX, valY)
   changeBg(`rgb(${valX}, ${valY}, 90)`)
 }
-
-// onmousemove = (e) => { 
-//   console.log("mouse location:", e.clientX, e.clientY)
-//   changeBg(`rgb(${e.clientX}, 50, 50)`)
-// }
-class App extends Component {
-  constructor() {
-    super() // whenever you extend a class, you need super to access it
-    this.state = {
-      people: people,
-      searchfield: ''
-    }
-  }
-
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-  }
-
-  render() {
-    const filteredCatpeople = this.state.people.filter(people =>{
-      return people.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-    })
-    return (
-      <div className="tc">
-        <h1 className="f3 f-headline-l">C A T  P E O P L E</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList people={ filteredCatpeople }/>
-      </div>
-    );
-  }
-
-  }
 
   export default App;
