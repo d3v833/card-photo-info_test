9cb30cf22d0a5ac564364bde84eff97ee8ab2ed3
d3v833 <d3v833@gmail.com>
2019-05-03 19:47:51 +0800

    adding some crazy css color stuff

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 import CardList from './cardList';
 import SearchBox from './searchBox';
 import { people } from './people';
+import './index.css';
 
+const changeBg = (color) => {
+  document.documentElement.style.setProperty('--background', color)
+}
+
+onmousemove = (e) => { 
+  let valX = (e.clientX / document.body.clientWidth) * 255
+  let valY = (e.clientY / document.body.clientHeight) * 255
+  console.log(valX, valY)
+  changeBg(`rgb(${valX}, ${valY}, 90)`)
+}
+
+// onmousemove = (e) => { 
+//   console.log("mouse location:", e.clientX, e.clientY)
+//   changeBg(`rgb(${e.clientX}, 50, 50)`)
+// }
 class App extends Component {
   constructor() {
     super() // whenever you extend a class, you need super to access it
--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
+
 ReactDOM.render(<App />,document.getElementById('root'));
 serviceWorker.register();
